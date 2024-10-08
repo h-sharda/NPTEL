f0b5e4d5975bbfad27fa5c59e8979c056a031af4
vector <lamexd097@gmail.com>
2024-10-09 01:24:57 +0530

    Updating Codes Again

--- src/W11_P5.java
+++ src/W11_P5.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 import java.lang.*;
 public class W11_P5 {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         try {
             Connection conn = null;
             Statement stmt = null;
