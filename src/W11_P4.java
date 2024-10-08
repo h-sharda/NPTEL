d1b7d7dd73f3c8f9fe313d54a05ab92e02e793ce
vector <lamexd097@gmail.com>
2024-10-09 01:23:06 +0530

    Updating Codes

--- src/W11_P4.java
+++ src/W11_P4.java
@@ -1,2 +1,30 @@
+import java.sql.*;
+import java.lang.*;
 public class W11_P4 {
+    public static void main(String args[]) {
+        try {
+            Connection conn = null;
+            Statement stmt = null;
+            String DB_URL = "jdbc:sqlite:/tempfs/db";
+            System.setProperty("org.sqlite.tmpdir", "/tempfs");
+
+            // Open a connection
+            conn = DriverManager.getConnection(DB_URL);
+            stmt = conn.createStatement();
+
+            // The statement containing SQL command to create table "STUDENTS"
+            // Execute the statement containing SQL command below this comment
+
+            String createTableCommand = "CREATE TABLE IF NOT EXISTS STUDENTS ("
+                    + "UID INT PRIMARY KEY NOT NULL, "
+                    + "Name VARCHAR(255), "
+                    + "Roll VARCHAR(255), "
+                    + "Age INT)";
+
+            stmt.executeUpdate(createTableCommand);
+            stmt.close();
+
+        }
+        catch(Exception e){ System.out.println(e);}
+    }
 }
--- src/W11_P5.java
+++ src/W11_P5.java
@@ -1,2 +1,24 @@
+import java.sql.*;
+import java.lang.*;
 public class W11_P5 {
+    public static void main(String args[]) {
+        try {
+            Connection conn = null;
+            Statement stmt = null;
+            String DB_URL = "jdbc:sqlite:/tempfs/db";
+            System.setProperty("org.sqlite.tmpdir", "/tempfs");
+            // Open a connection
+            conn = DriverManager.getConnection(DB_URL);
+            stmt = conn.createStatement();
+
+            // Write the SQL command to rename a table
+
+            // Execute the SQL command
+
+            String command = "ALTER TABLE STUDENTS RENAME TO GRADUATES;";
+            stmt.executeUpdate(command);
+            stmt.close();
+
+        }   catch(Exception e){ System.out.println(e);}
+    }
 }
